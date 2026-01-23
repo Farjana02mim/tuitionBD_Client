98a8bb3c88e46b7d65a437399277446502107f31
Farjana Akter Mim <farjanaaktermim330@gmail.com>
2026-01-23 20:17:51 +0600

    added navbar and footer section

---
+++ src/components/Logo/Logo.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import logo from '../../assets/logo-flat.png'
+export default function Logo() {
+  return (
+    <div className='h-[50px] w-[50px]'>
+      <img src={logo}></img>
+    </div>
+  )
+}
